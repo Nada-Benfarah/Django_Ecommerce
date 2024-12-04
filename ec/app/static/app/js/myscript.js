82101c64e4b1d5a2e8ab2885e4df45d90d901aa6
yassineraddaoui <yassineraddaouicontact@gmail.com>
2024-12-04 21:00:23 +0100

    god damn

--- ec/app/static/app/js/myscript.js
+++ ec/app/static/app/js/myscript.js
@@ -56,25 +56,31 @@ $('.minus-cart').click(function(){
     })
 })
 
-
 $('.remove-cart').click(function(){
-    var id=$(this).attr("pid").toString();
-    var eml=this
+    var id = $(this).attr("pid").toString();
+    var eml = this;
+    
     $.ajax({
-        type:"GET",
-        url:"/removecart",
-        data:{
-            prod_id:id
+        type: "GET",
+        url: "/removecart",  // Ensure this is the correct URL for the remove cart action
+        data: {
+            prod_id: id
         },
-        success:function(data){
-            document.getElementById("amount").innerText='TND.'+data.amount 
-            document.getElementById("totalamount").innerText=data.totalamount
-            eml.parentNode.parentNode.parentNode.parentNode.remove() 
+        success: function(data) {
+            $("#amount").text('TND. ' + data.amount.toFixed(2));  
+            $("#totalamount").text('TND. ' + data.totalamount.toFixed(2));  
+
+            $(eml).closest('.row').remove(); 
+
+            if ($("#cart").children().length === 0) {
+                $("#cart").html("<p>Cart is empty</p>");
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("Error removing item from cart:", error);
         }
-    })
+    });
 })
-
-
 $('.plus-wishlist').click(function(){
     var id=$(this).attr("pid").toString();
     $.ajax({
